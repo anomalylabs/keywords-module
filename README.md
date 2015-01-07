1a3eaf4d46ac8906c068e44e9e014634052008c4
Ryan Thompson <service@aiwebsystems.com>
2015-01-06 23:26:28 -0600

    Keywords

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# keywords-module
+# Keywords Module
+
 System wide keywords.
---
+++ src/KeywordsModule.php
@@ -0,0 +1,22 @@
+<?php namespace Anomaly\KeywordsModule;
+
+use Anomaly\Streams\Platform\Addon\Module\Module;
+
+/**
+ * Class KeywordsModule
+ *
+ * @link          http://anomaly.is/streams-platform
+ * @author        AnomalyLabs, Inc. <hello@anomaly.is>
+ * @author        Ryan Thompson <ryan@anomaly.is>
+ * @package       Anomaly\KeywordsModule
+ */
+class KeywordsModule extends Module
+{
+
+    /**
+     * The module navigation role.
+     *
+     * @var string
+     */
+    protected $navigation = 'streams::navigation.data';
+}
